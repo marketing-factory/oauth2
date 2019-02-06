4b6a3e40fa7372bd910be30b5845dae936b59b93
Christian Spoo <cs@marketing-factory.de>
2019-02-06 22:05:58 +0100

    Bump TYPO3 9 compatibility to 9.5.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'system',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.4.99'
+            'typo3' => '8.7.0-9.5.99'
         ],
         'conflicts' => [
         ],
