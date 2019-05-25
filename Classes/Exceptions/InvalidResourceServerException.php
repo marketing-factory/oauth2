f42570558d5a5a740e2f2678dfa1429483921857
Rennkadse <pr0@meetfurs.de>
2019-05-25 23:54:41 +0200

    [Feature] Allow registration of custom providers

---
+++ Classes/Exceptions/InvalidResourceServerException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * InvalidResourceServerException.php
+ *
+ * @author Fabian Bettag
+ * @date   25.05.2019
+ */
+
+namespace Mfc\OAuth2\Exceptions;
+
+class InvalidResourceServerException extends \Exception
+{
+
+}
---
+++ Classes/Exceptions/NotRegisteredException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * NotRegisteredException.php
+ *
+ * @author Fabian Bettag
+ * @date   25.05.2019
+ */
+
+namespace Mfc\OAuth2\Exceptions;
+
+class NotRegisteredException extends \Exception
+{
+
+}
