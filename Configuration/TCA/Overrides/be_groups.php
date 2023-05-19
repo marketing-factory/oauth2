df76e4525d5a0390748072afae812ab434460d5f
Sebastian Fischer <typo3@evoweb.de>
2023-05-19 16:23:09 +0200

    Fix assignment in gitlabGroup items

--- Configuration/TCA/Overrides/be_groups.php
+++ Configuration/TCA/Overrides/be_groups.php
@@ -1,30 +1,26 @@
 <?php
+
 defined('TYPO3') || die();
 
 call_user_func(function () {
-
-    $ll = 'LLL:EXT:oauth2/Resources/Private/Language/locallang.xlf:';
-
     $columns = [
         'gitlabGroup' => [
             'exclude' => 1,
-            'label' => $ll . 'columnLabel.gitlabGroup',
+            'label' => 'LLL:EXT:oauth2/Resources/Private/Language/locallang.xlf:columnLabel.gitlabGroup',
             'config' => [
                 'type' => 'select',
                 'renderType' => 'selectCheckBox',
                 'items' => [
-                    ['Guest', 10],
-                    ['Reporter', 20],
-                    ['Developer', 30],
-                    ['Master', 40],
-                    ['Owner', 50],
+                    ['label' => 'Guest', 'value' => 10],
+                    ['label' => 'Reporter', 'value' => 20],
+                    ['label' => 'Developer', 'value' => 30],
+                    ['label' => 'Master', 'value' => 40],
+                    ['label' => 'Owner', 'value' => 50],
                 ],
             ],
         ],
     ];
 
-
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_groups', $columns);
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('be_groups', 'gitlabGroup');
-
 });
