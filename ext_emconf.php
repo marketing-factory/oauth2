99fd953905eed7123c3e3e4075e9a5d7152587ca
Sebastian Klein <sebastian.klein@marketing-factory.de>
2025-04-24 11:19:25 +0200

    [TASK] Change version requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF['oauth2'] = [
     'version' => '3.3.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-13.99.99'
+            'typo3' => '13.4.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
