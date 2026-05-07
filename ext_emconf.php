5b7bcaedc3cffae93bdc2e763ea45a82e29d5f1d
Christian Spoo <christian.spoo@marketing-factory.de>
2026-05-07 13:31:39 +0200

    [TASK] Add PHP dependency to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,6 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.1.1',
     'constraints' => [
         'depends' => [
+            'php' => '8.2.0-8.5.99',
             'typo3' => '13.4.0-14.4.99',
         ],
         'conflicts' => [],
