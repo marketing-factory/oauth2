3335434fd7bab3d835d2687d364a385097dc9978
Christian Spoo <christian.spoo@marketing-factory.de>
2026-05-07 12:25:28 +0200

    [TASK] Remove author_company and re-add strict_types definition to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the package mfc/oauth2.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
-
 /** @noinspection PhpUndefinedVariableInspection */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'OAuth2 authentication and authorization',
     'description' => 'Generic OAuth 2.0 authentication and authorization for TYPO3 CMS',
     'category' => 'system',
-    'state' => 'stable',
     'author' => 'Christian Hellmund, Sebastian Klein, Simon Schmidt, Karoline Steinfatt, Christian Spoo',
     'author_email' => 'info@marketing-factory.de',
-    'author_company' => 'Marketing Factory Digital GmbH',
+    'state' => 'stable',
     'version' => '4.1.1',
     'constraints' => [
         'depends' => [
