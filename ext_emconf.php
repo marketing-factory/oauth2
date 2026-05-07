eddc5452396b4d69ea6a7f575f9f553108d9961d
Christian Spoo <christian.spoo@marketing-factory.de>
2026-05-07 11:29:26 +0200

    [TASK] Bump version in ext_emconf and replace extension key with $_EXTKEY

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,21 +3,21 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package mfd/typo3-fal-checker.
+ * This file is part of the package mfc/oauth2.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
 
-$EM_CONF['oauth2'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'OAuth2 authentication and authorization',
     'description' => 'Generic OAuth 2.0 authentication and authorization for TYPO3 CMS',
     'category' => 'system',
     'state' => 'stable',
     'author' => 'Christian Hellmund, Sebastian Klein, Simon Schmidt, Karoline Steinfatt, Christian Spoo',
-    'author_email' => 'typo3@marketing-factory.de',
+    'author_email' => 'info@marketing-factory.de',
     'author_company' => 'Marketing Factory Digital GmbH',
-    'version' => '3.3.1',
+    'version' => '4.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-14.4.99',
