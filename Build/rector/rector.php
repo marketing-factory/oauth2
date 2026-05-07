bd0a05cda8d9629bcd044233563aa79806f324dc
Christian Spoo <christian.spoo@marketing-factory.de>
2026-05-07 13:36:00 +0200

    [TASK] Let Rector keep the PHP version constraint

--- Build/rector/rector.php
+++ Build/rector/rector.php
@@ -42,6 +42,7 @@ return RectorConfig::configure()
     ])
     ->withPHPStanConfigs([Typo3Option::PHPSTAN_FOR_RECTOR_PATH])
     ->withConfiguredRule(ExtEmConfRector::class, [
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.5.99',
         ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '13.4.0-14.4.99',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ])
